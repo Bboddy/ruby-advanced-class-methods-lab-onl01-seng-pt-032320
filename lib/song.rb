78798ce5c543fe787083b2aaff354c0951a87231
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-18 04:39:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,48 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = Song.new 
+    song.save
+    return song
+  end
+  
+  def self.new_by_name(songName)
+    song = self.new 
+    song.name = songName
+    return song
+  end
+  
+  def self.create_by_name(songName)
+    song = self.create
+    song.name = songName
+    return song
+  end
+  
+  def self.find_by_name(songSearch)
+    self.all.find{ |song|
+      if song.name == songSearch
+        return song
+      end
+    }
+  end
+  
+  def self.find_or_create_by_name(songSearch)
+    if self.find_by_name(songSearch)
+      return self.find_by_name(songSearch)
+    else
+      self.create_by_name(songSearch)
+    end
+  end
+  
+  def self.alphabetical
+    self.all.sort_by{|song|
+      song.name.downcase
+    }
+  end
+  
+  def self.new_from_filename
+    
+  end
 end
